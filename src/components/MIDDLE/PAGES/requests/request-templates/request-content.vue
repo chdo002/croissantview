6f4d786d17da5cc6b3b5ad1a5d4b9487c4ebe242
chdo002 <1107661983@qq.com>
2019-11-08 22:31:54 +0800

    update

--- src/components/MIDDLE/PAGES/requests/request-templates/request-content.vue
+++ src/components/MIDDLE/PAGES/requests/request-templates/request-content.vue
@@ -1,39 +1,82 @@
 <template>
-    <div class="back">
-
-        <div class="temp">
+  <div class="back">
+    <!-- <div class="temp">
             {{currentRequest.text.request}}
+            <br><br>
+            {{currentRequest.text.error}}
         </div>
         <div class="temp">
             {{currentRequest.text.response}}
         </div>
         <div class="temp">
-            {{currentRequest.text.error}}
-        </div>
-    </div>
+            
+            
+    </div>-->
 
+    <ul>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+
+      <li>
+        22
+        <ul>
+          <li>12</li>
+          <li>12</li>
+          <li>13</li>
+          <li>12</li>
+          <li>13</li>
+          <li>
+            关键点
+            <ul v-if="true">
+              <li>12</li>
+              <li>13</li>
+              <li>
+                13
+                <ul>
+                  <li>13</li>
+                </ul>
+              </li>
+              <li>12</li>
+              <li>13</li>
+            </ul>
+          </li>
+          <li>结束</li>
+          <li>12</li>
+          <li>13</li>
+        </ul>
+      </li>
+
+      <li>14</li>
+      <li>14</li>
+      <li>14</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            request: Object
-        },
-        computed: {
-            // 计算属性的 getter
-            currentRequest: function () {
-                return this.$store.state.currentRequest;
-            }
-        }
+export default {
+  props: {
+    request: Object
+  },
+  computed: {
+    // 计算属性的 getter
+    currentRequest: function() {
+      return this.$store.state.currentRequest;
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .back{
-        display: flex;
-        flex-direction: column;
-    }
-    .temp {
-        padding-top: 10px;
-    }
+.back {
+  //   display: flex;
+  //   flex-direction: row;
+}
+li:nth-child(2n + 1) {
+  background: #39f;
+}
+.temp {
+  padding-top: 10px;
+}
 </style>
