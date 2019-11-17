37ca46be004cf37e8510d9a93389e7b716e1841d
chdo002 <1107661983@qq.com>
2019-11-18 00:06:08 +0800

    update

--- src/components/MIDDLE/PAGES/requests/request-templates/request-content.vue
+++ src/components/MIDDLE/PAGES/requests/request-templates/request-content.vue
@@ -1,43 +1,46 @@
 <template>
   <div>
-    <el-tabs @tab-click="handleTabClick" style="padding-left:10px;padding-right:10px">
+    <el-tabs
+      v-model="activeName"
+      @tab-click="handleTabClick"
+      style="padding-left:10px;padding-right:10px"
+    >
       <el-tab-pane label="request" name="first">
-        <vue-json-pretty
-          :data="currentRequest.text.request"
-          :highlightMouseoverNode="true"
-          @click="handleClick"
-        ></vue-json-pretty>
+        <HttpView :httpObj="currentRequest.text.request"></HttpView>
       </el-tab-pane>
 
       <el-tab-pane label="response" name="second">
-        <vue-json-pretty
-          :data="currentRequest.text.response"
-          :highlightMouseoverNode="true"
-          @click="handleClick"
-        ></vue-json-pretty>
+        <div v-if="currentRequest.text.response !== undefined">
+          <HttpView :httpObj="currentRequest.text.response"></HttpView>
+        </div>
+        <div v-else>空</div>
       </el-tab-pane>
 
-      <el-tab-pane label="error" name="fourth">
-        <vue-json-pretty
-          :data="currentRequest.text.error"
-          :highlightMouseoverNode="true"
-          @click="handleClick"
-        ></vue-json-pretty>
+      <el-tab-pane v-if="currentRequest.text.error !== undefined" label="error" name="third">
+        <div v-if="currentRequest.text.error !== undefined">
+          <vue-json-pretty
+            :data="currentRequest.text.error"
+            :highlightMouseoverNode="true"
+            @click="handleClick"
+          ></vue-json-pretty>
+        </div>
+        <div v-else>空</div>
       </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <script>
-import VueJsonPretty from "vue-json-pretty";
+
+import HttpView from "@/components/MIDDLE/PAGES/requests/request-templates/request-view.vue";
 
 export default {
   components: {
-    VueJsonPretty
+    HttpView
   },
   data() {
     return {
-      activeName: "second"
+      activeName: "first"
     };
   },
   props: {
@@ -53,7 +56,7 @@ export default {
     handleTabClick(tab, event) {
       console.log(tab, event);
     },
-    handleClick(param){
+    handleClick(param) {
       console.log(param + "?????");
     }
   }
---
+++ src/components/MIDDLE/PAGES/requests/request-templates/request-view.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <vue-json-pretty
+      :data="httpObj"
+      :highlightMouseoverNode="true"
+      @click="handleClick"
+      @change="handleChange"
+    ></vue-json-pretty>
+    <vue-json-pretty v-if="selectContent !== undefined" :data="selectContent"></vue-json-pretty>
+  </div>
+</template>
+
+<script>
+import VueJsonPretty from "vue-json-pretty";
+
+export default {
+  components: {
+    VueJsonPretty
+  },
+  data () {
+      return {
+          selectContent:undefined
+      }
+  },
+  props: {
+    httpObj: Object
+  },
+  updated() {
+    this.selectContent = httpObj.httpBody;
+  },
+  methods: {
+    handleClick(path, data, treeName = "") {
+      //   this.itemPath = path
+      //   this.itemData = !data ? data + '' : data // 处理 data = null 的情况
+      this.selectContent = data;
+    },
+    handleChange(newVal, oldVal) {
+      console.log("newVal: ", newVal, " oldVal: ", oldVal);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
